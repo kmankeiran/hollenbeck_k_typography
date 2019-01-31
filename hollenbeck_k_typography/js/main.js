b972db4d0b07c46264cfb30e112706500efc484b
kmankeiran <43183043+kmankeiran@users.noreply.github.com>
2019-01-30 22:12:32 -0500

    Add files via upload

---
+++ hollenbeck_k_typography/js/main.js
@@ -0,0 +1,13 @@
+//create jar; name jar, place string in jar
+var firstName = "Keiran";
+
+//alert is method(built in function in JS)
+//alert("Hello" + firstName);
+
+//modern JS
+//alert(`Hello ${firstName}`);
+
+//console.log("Hello" + firstName);
+//console.log("Hello" + " " + firstName);
+
+console.log("javascript linked up");
